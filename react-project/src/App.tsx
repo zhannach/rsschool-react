130452caf594622bf415e959bbdb65310d34d12a
zhannach <zhanna.chaykovska@gmail.com>
2023-03-07 12:38:39 +0200

    feat: set up react-router

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
 
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
-
   return (
-    <div > 
-    </div>
-  )
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About Us</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
