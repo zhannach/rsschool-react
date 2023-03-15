88d6191fc07b7789ab59bf12025b3e439f238a7d
zhannach <zhanna.chaykovska@gmail.com>
2023-03-15 13:12:32 +0200

    feat: add basic styles for pages

--- react-project/src/pages/About.tsx
+++ react-project/src/pages/About.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import styles from '../assets/styles/About.module.scss';
+
 export default class About extends React.Component {
   render() {
-    return <h1>Its about us page</h1>;
+    return (
+      <section className={styles.container}>
+        <h1 className={styles.title}>About us</h1>;
+      </section>
+    );
   }
 }
--- react-project/src/pages/NotFound.tsx
+++ react-project/src/pages/NotFound.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styles from '../assets/styles/NotFound.module.scss';
+
 export default class NotFound extends React.Component {
   render() {
     return (
-      <div>
-        <h2>
-          This page doesn't exist. Back to <Link to="/">Home</Link>
+      <div className={styles.container}>
+        <h2 className={styles.title}>
+          This page doesn&apos;t exist. Back to&nbsp;
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
         </h2>
       </div>
     );
