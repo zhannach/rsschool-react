0519edf99d4db4e4e81277df69964452b718c91a
zhannach <zhanna.chaykovska@gmail.com>
2023-03-23 10:39:17 +0200

    feat: change switch input to radio type, add ref to form inputs

--- react-project/src/pages/FormPage.tsx
+++ react-project/src/pages/FormPage.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { createRef } from 'react';
 
 import styles from '../assets/styles/Form.module.scss';
 
 export default class FormPage extends React.Component {
+  authorRef = createRef<HTMLInputElement>();
+  publishRef = createRef<HTMLInputElement>();
+  langRef = createRef<HTMLSelectElement>();
+  subscribeAuthorRef = createRef<HTMLInputElement>();
+  subscribeGenreRef = createRef<HTMLInputElement>();
+  coverRef = createRef<HTMLInputElement>();
+  imgFileRef = createRef<HTMLInputElement>();
+  formRef = createRef<HTMLFormElement>();
+
   render() {
     return (
       <div className={styles.container}>
         <section className={styles['form-content']}>
           <h1 className={styles.title}>Order book</h1>
-          <form className={styles.form}>
+          <form className={styles.form} ref={this.formRef}>
             <label>
               Author:
-              <input type="text" name="author"></input>
+              <input ref={this.authorRef} type="text" name="author" placeholder="Full name"></input>
             </label>
             <label>
               Publish date:
-              <input type="date" name="date"></input>
+              <input ref={this.publishRef} type="date" name="publishDate"></input>
             </label>
             <label htmlFor="lang">
-              Launguage
-              <select id="lang">
+              Language
+              <select id="lang" name="language">
+                <option value=""></option>
                 <option value="English">English</option>
                 <option value="French">French</option>
                 <option value="Spanish">Spanish</option>
-                <option value="Ukraine">Ukrainian</option>
+                <option value="Ukrainian">Ukrainian</option>
                 <option value="Italian">Italian</option>
                 <option value="German">German</option>
                 <option value="Russian">Russian</option>
@@ -32,24 +42,29 @@ export default class FormPage extends React.Component {
             <label>
               Subscribe to:
               <label>
-                <input type="checkbox"></input>
+                <input ref={this.subscribeAuthorRef} type="checkbox" name="subscribeAuthor"></input>
                 this author
               </label>
               <label>
-                <input type="checkbox"></input>
+                <input ref={this.subscribeGenreRef} type="checkbox" name="subscribeGenre"></input>
                 this genre
               </label>
             </label>
-            <label>
-              Softcover / Hardcover
-              <label className={styles.switch}>
-                <input type="checkbox"></input>
+            <label className={styles.cover}>
+              Book cover:
+              <label>
+                <input ref={this.coverRef} type="radio" name="cover"></input>
+                Softcover
+              </label>
+              <label>
+                <input ref={this.coverRef} type="radio" name="cover"></input>
+                Hardcover
                 <span className={styles.slider}></span>
               </label>
             </label>
             <label>
               Upload book cover:
-              <input type="file" />
+              <input ref={this.imgFileRef} type="file" name="img" />
             </label>
             <button type="submit">Submit</button>
           </form>
