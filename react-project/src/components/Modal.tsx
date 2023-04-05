100bb331dfaba1b021acb66faa0163c7d19e728d
zhannach <zhanna.chaykovska@gmail.com>
2023-04-05 20:25:05 +0300

    feat: add modal card

---
+++ react-project/src/components/Modal.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import style from '../assets/styles/Home.module.scss';
+
+import { Book } from 'types/home';
+
+const Modal = (props: { card?: Book; onCloseModal: () => void }) => {
+  const { card } = props;
+  return (
+    <div className={style.modal} onClick={() => props.onCloseModal()}>
+      <div className="modal-dialog">
+        {card && (
+          <div className={style['modal-content']} onClick={(e) => e.stopPropagation()}>
+            <span className={style.close} onClick={() => props.onCloseModal()}>
+              &times;
+            </span>
+            <div className={style['modal-card']}>
+              <img
+                src={`${card.imageLinks.thumbnail}&fife=w400-h600`}
+                className={style['modal-img']}
+              ></img>
+              <div className={style['info-section']}>
+                <h3 className={style.info}>
+                  <p className={style['info-text']}>Title: </p>
+                  {card.title}
+                </h3>
+                <span className={style.info}>
+                  <p className={style['info-text']}>Author: </p>
+                  {card.authors}
+                </span>
+                <span className={style.info}>
+                  <p className={style['info-text']}> Categories: </p>
+                  {card.categories}
+                </span>
+                <span className={style.info}>
+                  <p className={style['info-text']}>Language: </p>
+                  {card.language}
+                </span>
+                <span className={style.info}>
+                  <span className={style['info-text']}>Year: </span>
+                  {card.publishedDate}
+                </span>
+                <span className={style.info}>
+                  <span className={style['info-text']}>Pages: </span>
+                  {card.pageCount}
+                </span>
+                <div className={style.btns}>
+                  <button className={style['btn-buy']}>Buy</button>
+                  <button className={style['btn-add']}>Add</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
--- react-project/src/helpers/fetchCard.ts
+++ react-project/src/helpers/fetchCard.ts
@@ -1,12 +1,23 @@
-import { BookResponse } from 'types/home';
+import { BookResponse, BookData } from 'types/home';
 
 export const fetchCards = async (value: string) => {
   try {
     const response = await fetch(
-      `https://www.googleapis.com/books/v1/volumes?q=${value}&maxResults=30`
+      `https://www.googleapis.com/books/v1/volumes?q=${value || 'javascript'}&maxResults=30`
     );
     const books = (await response.json()) as BookResponse;
-    return books.items;
+    const filterData = books.items.filter((el) => el.volumeInfo.imageLinks);
+    return filterData;
+  } catch (error) {
+    throw Error('something went wrong');
+  }
+};
+
+export const fetchCard = async (id: string) => {
+  try {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes/${id}`);
+    const book = (await response.json()) as BookData;
+    return book;
   } catch (error) {
     throw Error('something went wrong');
   }
