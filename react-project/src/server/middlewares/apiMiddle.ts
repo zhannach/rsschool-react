93abca6fa0ade42fdecb78697decb899839ee4f5
zhannach <zhanna.chaykovska@gmail.com>
2023-05-01 22:15:22 +0300

    fix: fix ssr

--- react-project/src/server/middlewares/apiMiddle.ts
+++ react-project/src/server/middlewares/apiMiddle.ts
@@ -2,7 +2,7 @@ import { Store } from 'client/redux/store';
 import { booksApi } from '../../client/redux/booksApi';
 
 const apiRequest = async (store: Store) => {
-  store.dispatch(booksApi.endpoints.getCards.initiate('react'));
+  store.dispatch(booksApi.endpoints.getCards.initiate(''));
 
   return await Promise.all(store.dispatch(booksApi.util.getRunningQueriesThunk()));
 };
